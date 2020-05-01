aea9551817429890d4639b2a0c14dbc03db3ca6c
Sophie Cosgrove <sophie.cosgrovee@hotmail.co.uk>
2020-05-01 12:41:06 +0100

    4th task

---
+++ Iterations.py
@@ -0,0 +1,17 @@
+count = 0
+name = str(input('What is your name? '))
+
+while count < 5:
+    print(name, 'is awesome!')
+    count += 1
+
+for i in range(5,11):
+    print(i)
+
+count = 0 
+
+while count < 5:
+    name1 = str(input('What is your name? '))
+    print(name1 + ' is awesome!')
+    count += 1
+
