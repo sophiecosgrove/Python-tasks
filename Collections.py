84e35be740750b51115263ebb9da991a3ecc24d8
Sophie Cosgrove <sophie.cosgrovee@hotmail.co.uk>
2020-05-01 12:16:04 +0100

    'first 2 exercises'

---
+++ Collections.py
@@ -0,0 +1,2 @@
+books = {'He said She said': 'Erin Kelly', 'The Starless Sea':'Erin Morgenstein', 'The End of Everything':'Megan Abbot'}
+print(books['The Starless Sea'])
---
+++ Datatypes.py
@@ -0,0 +1,29 @@
+print('veggies are good ' + '4 ' + 'you')
+
+word1 = 'Good'
+word2 = 'Day'
+word3 = 'Sophie'
+
+sentence = word1 + ' ' + word2 + ' ' + word3
+
+print(sentence)
+
+number1 = input('Enter whole number: ')
+number2 = input('Enter decimal number: ')
+
+integer_number = int(number1)
+float_number = float(number2)
+round_number = int(round(float_number))
+
+print(number1)
+print(number2)
+print(round_number)
+
+name = 'Pep Guardogiola'
+age = 3
+bark = True
+tweet = False
+
+print('My pet is called', name, ', He is', age, 'years old.')
+print('Statement:', name, 'barks.' , bark)
+print('Statement:', name, 'tweets.', tweet)
